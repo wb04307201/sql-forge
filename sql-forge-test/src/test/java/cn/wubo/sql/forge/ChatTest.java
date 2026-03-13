9baa65751691dd370d8dfb115a2ffb9a27c94173
吴博 <wubo_aaa@163.com>
2026-03-13 11:07:15 +0800

    refactor(test): 移除 ChatTest 中的 prompt 测试方法
    - 删除了 prompt 测试方法及其相关实现代码
    - 移除了 SQL Forge 属性配置和模板替换逻辑
    - 清理了 JSON 表结构示例数据
    - 简化了测试类的整体结构

--- sql-forge-test/src/test/java/cn/wubo/sql/forge/ChatTest.java
+++ sql-forge-test/src/test/java/cn/wubo/sql/forge/ChatTest.java
@@ -32,43 +32,4 @@ class ChatTest {
         flux.subscribe(log::info);
         Thread.sleep(5000);
     }
-
-
-    @Test
-    void prompt() {
-        SqlForgeProperties sqlForgeProperties = new SqlForgeProperties();
-        String template = sqlForgeProperties.getAi().getPromptTemplate();
-        template = template.replace("{{API_SPEC}}", sqlForgeProperties.getAi().getApiSpec());
-        template = template.replace("{{TABLE_INFO}}", """
-```json
-[{
-  "table": "PRODUCTS",
-  "desc": "商品表",
-  "type": "crud",
-  "fields": {
-    "ID": {"type": "uuid", "desc": "商品ID"},
-    "NAME": {"type": "string", "length": 50, "desc": "商品名称","search": true},
-    "DICT_CATEGORIES": {"type": "dict", "length": 100, "desc": "商品类型", "ref": {"type":"JOIN","table": "sys_dict_items", "on": "item_code", "filter": {"dict_code": "categories"}},"search": true},
-    "PRICE": {"type": "number", "max": 9999999999, "precision": 2, "desc": "邮箱地址","search": true}
-  }
-},
-  {
-    "table": "SYS_DICT_ITEMS",
-    "desc": "字典项表",
-    "type": "dict",
-    "fields": {
-      "DICT_CODE": {"type": "string"},
-      "ITEM_CODE": {"type": "string"},
-      "ITEM_NAME": {"type": "string"}
-    }
-  }]
-```
-""");
-        template = template.replace("{{EXAMPLE_TABLE_INFO}}", sqlForgeProperties.getAi().getExampleTableInfo());
-        template = template.replace("{{EXAMPLE_AMIS_INFO}}", sqlForgeProperties.getAi().getExampleAmisInfo());
-
-
-        String result = chatClient.prompt().user(template).call().content();
-        log.info("result: {}", result);
-    }
 }
